88a7a14f1657526784f9f563f6e40a9732cfcf08
faizan45640 <faizan45640@gmail.com>
2024-05-31 20:17:43 +0500

    Add 2d linked list

---
+++ twod_linkedlist.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *below;
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+        below = nullptr;
+    }
+    Node()
+    {
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head = nullptr;
+
+    void matrixtolinkedlist(int arr[][3])
+    {
+
+        Node *heads[3];
+        Node *nexttemp, ptr;
+        heads[0] = NULL;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Node *ptr = new Node(arr[i][j]);
+                if (heads[i] == NULL)
+                {
+
+                    heads[i] = ptr;
+                }
+                else
+                {
+                    nexttemp->next = ptr;
+                }
+                nexttemp = ptr;
+            }
+        }
+
+        
+
+        for (int i = 0; i < 2; i++)
+        {
+            Node *temp1 = heads[i];
+        Node *temp2 = heads[i+1];
+            while (temp1 && temp2)
+            {
+                temp1->below = temp2;
+                temp1 = temp1->next;
+                temp2 = temp2->next;
+            }
+        }
+        head = heads[0];
+    }
+    void display(){
+        Node* right;
+        Node* down;
+        down=head;
+         
+        while(down){
+            cout<<endl;
+            right=down;
+            while(right){
+                cout<<right->data<<"->";
+                right=right->next;
+
+            }
+           
+            down=down->below;
+            
+        }
+
+    }
+};
+
+int main()
+{
+
+    int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    LinkedList l;
+    l.matrixtolinkedlist(arr);
+    l.display();
+   
+}
